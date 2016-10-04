3f06866ab12df81181f095313712d1bfb7069243
August Burns <shturm@urghh.net>
2016-10-04 21:17:12 +0300

    basic ng cli bootstrap

---
+++ src/app/category-panel/category-panel.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { CategoryPanelComponent } from './category-panel.component';
+
+describe('Component: CategoryPanel', () => {
+  it('should create an instance', () => {
+    let component = new CategoryPanelComponent();
+    expect(component).toBeTruthy();
+  });
+});
